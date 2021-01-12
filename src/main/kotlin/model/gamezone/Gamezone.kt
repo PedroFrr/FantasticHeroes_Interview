c580a1eeb49eb9f340d5cd41a81b79609fb53f63
PedroFrr <pedroandref96@gmail.com>
2021-01-12 01:57:50 +0000

    Add functionality for Characters to move on the map

--- src/main/kotlin/model/gamezone/Gamezone.kt
+++ src/main/kotlin/model/gamezone/Gamezone.kt
@@ -14,15 +14,17 @@ class Gamezone(val grid: Grid) {
     }
 
     //Moves Character (Heroes and Monsters) on the map after all the validations are good
-    //TODO validation
     fun moveCharacter(character: MapCharacter, direction: Directions) {
 
         val characterCellBeforeMoving = getCharacterCell(character)
-
-        //TODO validation before moving - if Character is at the edges of the gamezone or if there's already a Character on the Cell he will end up in
-        //TODO moveCharacter
-        //TODO emptyCellWhereCharacter was
-
+        if (characterCanMove(characterCellBeforeMoving, direction)) {
+            val characterCellAfterMoving = moveCharacterAndReturnCharacterPosition(character, direction)
+            setCharacterAtCell(characterCellAfterMoving, character)  //updates Cell with value for the given Character
+            //TODO empty Cell WhereCharacter was
+        } else {
+            //if character can't move do an early return (nothing happens)
+            return
+        }
 
     }
 
@@ -36,6 +38,42 @@ class Gamezone(val grid: Grid) {
 
     //Returns the Character cell
     fun getCharacterCell(character: MapCharacter): Cell = cellValues.filterValues { it == character }.keys.first()
+
+    //Returns true if Character can move on the map
+    //Being able to move means that the character isn't at the edge of the map
+    private fun characterCanMove(characterCell: Cell, direction: Directions): Boolean {
+
+        val characterXCoordinate = characterCell.xCoordinate
+        val characterYCoordinate = characterCell.yCoordinate
+        return when (direction) {
+            Directions.South -> characterYCoordinate != 0
+            Directions.North -> characterYCoordinate != grid.size
+            Directions.West -> characterXCoordinate != 0
+            Directions.East -> characterXCoordinate != grid.size
+        }
+    }
+
+    //moves Character on the Gamezone
+    //South and North move the Character on the y axis
+    //West and East move the Character on the x axis
+    //if the actual coordinates of the Character don't allow it to move, the position remains the same (we could return an error message, but I'm keeping it simple)
+    //the Cell where the Character ends up on the Gamezone is returned on this method
+    private fun moveCharacterAndReturnCharacterPosition(character: MapCharacter, direction: Directions): Cell {
+        val characterCell = getCharacterCell(character)
+        val xCoordinate = characterCell.xCoordinate
+        val yCoordinate = characterCell.yCoordinate
+
+        return when (direction) {
+            Directions.North -> characterCell.copy(yCoordinate = yCoordinate + 1)
+
+            Directions.East -> characterCell.copy(xCoordinate = xCoordinate + 1)
+
+            Directions.South -> characterCell.copy(yCoordinate = yCoordinate - 1)
+
+            Directions.West -> characterCell.copy(xCoordinate = xCoordinate - 1)
+
+        }
+    }
 
 
 
